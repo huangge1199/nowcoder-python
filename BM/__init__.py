7ae178e8727c23c3984af04269175b616f142848
轩辕龙儿 <hyy>
2022-03-01 16:07:36 +0800

    初始化项目

---
+++ BM/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# @author JourWon
+# @date 2022/3/1
+# @file __init__.py.py
---
+++ common/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+# @author 轩辕龙儿
+# @date 2022/3/1
+# @file __init__.py.py

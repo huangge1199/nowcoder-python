bde4b7fbf5590fe8d707366f761a82cb8dadec91
轩辕龙儿 <huangge1199@hotmail.com>
2022-10-26 10:46:12 +0800

    NP21 增加派对名单（二）

---
+++ NP/NP21.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# @author 轩辕龙儿
+# @date 2022/10/26
+# @file NP21.py
+
+def solution():
+    nameList = input().split()
+    nameList.insert(0, "Allen")
+    print(nameList)
+
+
+if __name__ == "__main__":
+    solution()

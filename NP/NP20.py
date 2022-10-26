01f838d55c06db5fa343615fb849fd9b8a043fe6
轩辕龙儿 <huangge1199@hotmail.com>
2022-10-26 10:43:17 +0800

    NP20 增加派对名单（一）

---
+++ NP/NP20.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# @author 轩辕龙儿
+# @date 2022/10/26
+# @file NP20.py
+
+def solution():
+    nameList = input().split()
+    nameList.append("Allen")
+    print(nameList)
+
+
+if __name__ == "__main__":
+    solution()

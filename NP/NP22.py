f0d2e1fd080af94774a8a5ea057c0562a3013037
轩辕龙儿 <huangge1199@hotmail.com>
2022-10-26 10:50:22 +0800

    NP22 删除简历

---
+++ NP/NP22.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# @author 轩辕龙儿
+# @date 2022/10/26
+# @file NP22.py
+
+def solution():
+    com = input().split()
+    com.remove(com[0])
+    print(com)
+
+
+if __name__ == "__main__":
+    solution()

d13aef71d8575956e251ce8d110edeb1e38f0f5c
轩辕龙儿 <huangge1199@hotmail.com>
2022-10-26 15:23:50 +0800

    NP26 牛牛的反转列表

---
+++ NP/NP26.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# @author 轩辕龙儿
+# @date 2022/10/26
+# @file NP26.py
+
+def solution():
+    num = [3, 5, 9, 0, 1, 9, 0, 3]
+    # print(num)
+    num.reverse()
+    print(num)
+
+
+if __name__ == "__main__":
+    solution()

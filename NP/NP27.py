f7e79be7e38b6c186c62aac6a42d765a542ae24c
轩辕龙儿 <huangge1199@hotmail.com>
2022-10-26 15:30:45 +0800

    NP27 朋友们的喜好

---
+++ NP/NP27.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# @author 轩辕龙儿
+# @date 2022/10/26
+# @file NP27.py
+
+def solution():
+    name = ["Niumei", "YOLO", "Niu Ke Le", "Mona"]
+    friends = []
+    friends.append(name)
+    food = ["pizza", "fish", "potato", "beef"]
+    friends.append(food)
+    number = [3, 6, 0, 3]
+    friends.append(number)
+    print(friends)
+
+
+if __name__ == "__main__":
+    solution()

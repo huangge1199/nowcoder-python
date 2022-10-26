a7e783e36174a897fc7d166d50d82a70ed599c93
轩辕龙儿 <huangge1199@hotmail.com>
2022-10-26 09:58:03 +0800

    NP17 生成列表

---
+++ NP/NP17.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# @author 轩辕龙儿
+# @date 2022/10/26
+# @file NP17.py
+
+def solution():
+    s = input()
+    namelist = s.split(' ')
+    print(namelist)
+
+
+if __name__ == "__main__":
+    solution()

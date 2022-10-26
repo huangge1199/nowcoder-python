78f8cedd2443a5ddbc9b3d734a3eb28372f009c2
轩辕龙儿 <huangge1199@hotmail.com>
2022-10-26 15:07:20 +0800

    NP25 有序的列表

---
+++ NP/NP25.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# @author 轩辕龙儿
+# @date 2022/10/26
+# @file NP25.py
+
+def solution():
+    my_list = ["P", "y", "t", "h", "o", "n"]
+    print(sorted(my_list))
+    print(my_list)
+    my_list.sort(reverse=True)
+    print(my_list)
+
+
+if __name__ == "__main__":
+    solution()

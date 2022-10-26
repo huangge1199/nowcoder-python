ba1b1f31ced8da15f153fae71d164c044146f77c
轩辕龙儿 <huangge1199@hotmail.com>
2022-10-26 10:32:02 +0800

    NP18 生成数字列表

---
+++ NP/NP18.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# @author 轩辕龙儿
+# @date 2022/10/26
+# @file NP18.py
+
+def solution():
+    print(list(map(int, input().split())))
+
+
+if __name__ == "__main__":
+    solution()

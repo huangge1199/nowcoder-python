210700b6dd2e92a303916a8614fb247ef63886cf
轩辕龙儿 <huangge1199@hotmail.com>
2022-10-26 15:57:42 +0800

    NP31 团队分组

---
+++ NP/NP31.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# @author 轩辕龙儿
+# @date 2022/10/26
+# @file NP31.py
+
+def solution():
+    group_list = ["Tom", "Allen", "Jane", "William", "Tony"]
+    print(group_list[0:2])
+    print(group_list[1:4])
+    print(group_list[3:5])
+
+
+if __name__ == "__main__":
+    solution()
